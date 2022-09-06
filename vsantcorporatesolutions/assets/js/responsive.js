f29d485d03933af7b1079f44b8a5188e03fd33f3
CTathva <ctathva@gmail.com>
2022-09-06 23:39:59 +0530

    1.0.000014

--- vsantcorporatesolutions/assets/js/responsive.js
+++ vsantcorporatesolutions/assets/js/responsive.js
@@ -21,10 +21,6 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -54,12 +50,23 @@ function carousel() {
 window.onload = function(){ 
   showSlides(1);
 
+
   let sleep = ms => {  
     return new Promise(resolve => setTimeout(resolve, ms));  
   };  
   sleep(7000).then(() => {  
     carousel();
   }); 
+  var el = document.getElementById('swipezone');
+  swipedetect(el, function(swipedir){
+    // swipedir contains either "none", "left", "right", "top", or "down"
+    if (swipedir==="left"){
+      showSlides(slideIndex += 1);
+    }
+    if (swipedir==="right"){
+      showSlides(slideIndex -= 1);
+    }
+});
   var hideMe = document.getElementById('companiesmenu');
   const display1 = window.getComputedStyle(hideMe).display; 
        document.onclick = function(e){
@@ -103,4 +110,51 @@ function changeAbout(n){
     om.classList.add("active");
 
   }
+}
+
+function swipedetect(el, callback){
+  
+  var touchsurface = el,
+  swipedir,
+  startX,
+  startY,
+  distX,
+  distY,
+  threshold = 150, //required min distance traveled to be considered swipe
+  restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 300, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime,
+  handleswipe = callback || function(swipedir){}
+
+  touchsurface.addEventListener('touchstart', function(e){
+      var touchobj = e.changedTouches[0]
+      swipedir = 'none'
+      dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime() // record time when finger first makes contact with surface
+      e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchmove', function(e){
+      e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+
+  touchsurface.addEventListener('touchend', function(e){
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (elapsedTime <= allowedTime){ // first condition for awipe met
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+              swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+          }
+      }
+      handleswipe(swipedir)
+      e.preventDefault()
+  }, false)
 }
